236d2d956240ce432cf992d5a09362978e402e7f
Vnovnick <safonov.nick10@gmail.com>
2022-08-10 17:44:00 -0700

    name change resize function

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,10 +1,28 @@
 import React, {FC} from "react";
 import './Sidebar.scss';
-import Nav from "react-bootstrap";
+
+
+
 
 const Sidebar: FC = () => {
+    window.onload = () => {
+        fullNameResize();
+    };
+    const fullNameResize = () => {
+        let winWidth: number = window.innerWidth;
+        if (winWidth < 1100){
+            (document.getElementById('fullName') as HTMLFormElement).innerHTML = 'Nick S.';
+        }else if (winWidth >= 1101){
+            (document.getElementById('fullName') as HTMLFormElement).innerHTML = 'Nicholas Safonov';
+        }
+    };
+
+    window.addEventListener('resize', () => {
+        fullNameResize();
+    })
   return (
     <div className="sidebar">
+        <h3 id="fullName">Nicholas Safonov</h3>
         <ul className="nav flex-column">
             <a className="nav-link" href="#">Bio</a>
             <a className="nav-link" href="#">Portfolio</a>
