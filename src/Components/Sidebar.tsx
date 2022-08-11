6ad2d6e61fbae0ba12405e2826bc5c172b236fcf
Vnovnick <safonov.nick10@gmail.com>
2022-08-10 17:12:12 -0700

    sidebar styling; adding sass + bootstrap

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,10 +1,17 @@
 import React, {FC} from "react";
 import './Sidebar.scss';
-
+import Nav from "react-bootstrap";
 
 const Sidebar: FC = () => {
   return (
-    <div>Sidebar</div>
+    <div className="sidebar">
+        <ul className="nav flex-column">
+            <a className="nav-link" href="#">Bio</a>
+            <a className="nav-link" href="#">Portfolio</a>
+            <a className="nav-link" href="#">Performance Videos</a>
+            <a className="nav-link" href="#">Misc</a>
+        </ul>
+    </div>
   )
 }
 
