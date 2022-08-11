1263a268ca6adbbb17d45bee1e78688e13dc318f
Vnovnick <safonov.nick10@gmail.com>
2022-08-10 22:36:08 -0700

    adding Main SiteContent folder; styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import Sidebar from './Components/Sidebar';
+import Sidebar from './Components/Sidebar/Sidebar';
+import SiteContent from './Components/SiteContent/SiteContent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Sidebar></Sidebar>
       </header>
+      <Sidebar/>
+      <SiteContent/>
     </div>
   );
 }
---
+++ src/Components/SiteContent/SiteContent.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import './SiteContent.scss';
+
+const SiteContent: FC = () => {
+    return(
+        <div className='site-content'>
+            SiteContent
+        </div>
+    );
+};
+
+export default SiteContent;
