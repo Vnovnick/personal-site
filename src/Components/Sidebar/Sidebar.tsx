05e42f2681e22fbaa02bc35530a844d128c67f4f
Vnovnick <safonov.nick10@gmail.com>
2022-08-15 16:34:03 -0700

    adding on select effect

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -8,6 +8,12 @@ const Sidebar: FC = () => {
     window.onload = () => {
         fullNameResize();
     };
+
+    // const selectSection = (target: string): void => {
+    //     const section = document.getElementById(target) as HTMLFormElement;
+    //     section.classList.toggle('selected');     
+    // };
+
     const fullNameResize = (): void => {
         let winWidth: number = window.innerWidth;
         if (winWidth < 1270){
@@ -24,10 +30,10 @@ const Sidebar: FC = () => {
     <div className="sidebar">
         <h3 id="fullName">Nicholas Safonov</h3>
         <ul className="nav flex-column">
-            <a className="nav-link" href="#">Bio</a>
-            <a className="nav-link" href="#">Portfolio</a>
-            <a className="nav-link" href="#">Performance Videos</a>
-            <a className="nav-link" href="#">Misc</a>
+            <a className="nav-link" id='nav-bio' href="#">Bio</a>
+            <a className="nav-link" id='nav-port' href="#">Portfolio</a>
+            <a className="nav-link" id='nav-perf' href="#">Performance Videos</a>
+            <a className="nav-link" id='nav-misc' href="#">Misc</a>
         </ul>
     </div>
   )
