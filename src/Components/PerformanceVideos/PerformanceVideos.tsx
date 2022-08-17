c5453635bae538b112d6399256649fba75ee701e
Vnovnick <safonov.nick10@gmail.com>
2022-08-16 18:30:43 -0700

    adding grid changes

--- src/Components/PerformanceVideos/PerformanceVideos.tsx
+++ src/Components/PerformanceVideos/PerformanceVideos.tsx
@@ -3,15 +3,16 @@ import './PerformanceVideos.scss'
 
 const PerformanceVideos: FC = () => {
     return (
-        <div className="performance-videos" id='performance-videos'>
-            <div className="vid-1 col-span-2">
-                <iframe className='embed-vid' src="https://www.youtube.com/embed/Z3sLFa3FkmI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        <div className="performance-videos block lg:grid" id='performance-videos'>
+            <div className="vid-1">
+                <iframe className='embed-vid w-full col-span-2' src="https://www.youtube.com/embed/Z3sLFa3FkmI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
             </div>
+            <p className="text-white lg:col-start-3">Bach Sonata No. 2 in A minor - Grave</p>
             <div className="vid-2">
-                <iframe className='embed-vid' src="https://www.youtube.com/embed/qP12tqO3ZdM" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                <iframe className='embed-vid w-full col-span-2' src="https://www.youtube.com/embed/qP12tqO3ZdM" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
             </div>
             <div className="vid-3">
-                <iframe className='embed-vid' src="https://www.youtube.com/embed/vPrTJeND3-k" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                <iframe className='embed-vid w-full col-span-2' src="https://www.youtube.com/embed/vPrTJeND3-k" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
             </div>
         </div>
     );
