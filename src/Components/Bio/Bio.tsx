5c5d126d91c19d71f8d935576c14805954fbaf9d
Vnovnick <safonov.nick10@gmail.com>
2022-08-29 15:59:33 -0700

    adding runTracker demo

--- src/Components/Bio/Bio.tsx
+++ src/Components/Bio/Bio.tsx
@@ -5,7 +5,7 @@ import image from './bioImage.jpg';
 const Bio: FC = () => {
     return(
         <div className='bio' id='bio'>        
-            <img src={image} className='float-right rounded-lg' id='bio-image'></img>
+            <img src={image} alt='profile' className='float-right rounded-lg' id='bio-image'></img>
             <p className ='text-white font-sans'>
             I am a former violinist, aspiring front-end developer, and freelance sound designer from Northern California. 
             I hold a Bachelor of Music (BM) degree from the IU Jacobs’ School of Music and am currently working on my Master of Music (MM) degree at the University of North Texas College of Music.
@@ -18,10 +18,8 @@ const Bio: FC = () => {
             I have also served as a substitute violinist for multiple orchestras in Indiana.
             I have studied with Grigory Kalinovsky at the Jacobs’ School of Music and am studying under Julia Bushkova at UNT.
              I have worked with or been coached by Pinchas Zukerman, Rosa Fain, Phillip Setzer, Nicholas Mann, and many others. 
-             Additionally, Nicholas I have had master classes with the Dover Quartet and Seth Knopp. 
-            </p>
-            
-            
+             Additionally, I have had master classes with the Dover Quartet and Seth Knopp. 
+            </p>                      
         </div>
     );
 };
--- src/Components/Portfolio/Portfolio.tsx
+++ src/Components/Portfolio/Portfolio.tsx
@@ -1,10 +1,14 @@
 import React, {FC} from "react";
 import './Portfolio.scss';
+import runvid from './RunTracker.mp4';
 
 const Portfolio: FC = () => {
     return (
-        <div className="portfolio">
+        <div className="portfolio block lg:grid">
             Portfolio
+            <div className="port-1">
+                <video src={runvid}></video>
+            </div>
         </div>
     );
 
