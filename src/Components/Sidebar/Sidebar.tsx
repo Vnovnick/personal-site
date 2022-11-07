e997ae1c8e89d206b494edcb43999859a99e8ddd
Vnovnick <nicholas@proppex.com>
2022-11-06 18:11:24 -0800

    more nav styling changes

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -29,18 +29,22 @@ export default function Sidebar() {
   });
   // got online
 
+  // const sidebarLinks = document.querySelectorAll(`[id$="link"]`);
+  // sidebarLinks.forEach((link) => {
+  //   link.addEventListener("click", (event) => {});
+  // });
+
   const navLinkFormatter = (link: string, linkName: string) => {
     return (
       <div
-        className={`w-full flex transition-all ${
-          location.pathname === link
-            ? "bg-green-900/50"
-            : "hover:bg-green-900/50"
+        className={`w-full flex group hover:bg-gradient-to-r hover:from-black hover:via-green-900/50 hover:bg-[length:600px_100px] bg-right transition-[background] duration-100 ${
+          location.pathname === link &&
+          "bg-left bg-gradient-to-r from-black via-green-900/50 bg-[length:700px_100px]"
         }`}
       >
         <p
-          className={`pl-3 transition-all ${
-            location.pathname === link && "ml-auto pr-7"
+          className={`pl-3 transition-all w-fit ${
+            location.pathname === link ? "pl-10" : "group-hover:tracking-wide"
           }`}
         >
           {linkName}
@@ -50,16 +54,18 @@ export default function Sidebar() {
   };
 
   return (
-    <div className="bg-black flex flex-col border h-screen border-white">
-      <p className="text-white text-3xl mx-auto" id="fullName">
+    <div className="bg-black flex flex-col h-screen">
+      <p className="text-white text-3xl mx-auto pt-5" id="fullName">
         Nicholas Safonov
       </p>
       <div className="flex flex-col text-green-400 text-lg h-1/5 justify-between mt-10 w-full">
-        <Link to="/bio">{navLinkFormatter("/bio", "Bio")}</Link>
-        <Link to="/portfolio">
+        <Link to="/bio" id="bio-link">
+          {navLinkFormatter("/bio", "Bio")}
+        </Link>
+        <Link to="/portfolio" id="portfolio-link">
           {navLinkFormatter("/portfolio", "Portfolio")}
         </Link>
-        <Link to="/performance-videos">
+        <Link to="/performance-videos" id="perf-videos-link">
           {navLinkFormatter("/performance-videos", "Performance Videos")}
         </Link>
         <p className="hover:bg-green-900/50 pl-3" id="nav-misc">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,tsx,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        'height': 'height',
+        'spacing': 'margin, padding',
+      },
+      boxShadow: {
+        "even-outer": '0 0px 16px 0px rgba(0, 0, 0, 0.3)',
+      }
+    },
   },
   plugins: [],
 }
