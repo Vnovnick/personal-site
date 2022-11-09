4ccf5eb63e37e7bd47c8c306bca699668e7654b5
Vnovnick <nicholas@proppex.com>
2022-11-09 09:15:26 -0800

    refactoring performance-vid page

--- src/Components/PerformanceVideos/PerformanceVideos.tsx
+++ src/Components/PerformanceVideos/PerformanceVideos.tsx
@@ -1,22 +1,41 @@
-import React, {FC} from "react";
-import './PerformanceVideos.scss'
+import React, { FC } from "react";
 
 const PerformanceVideos: FC = () => {
-    return (
-        <div className="performance-videos section block lg:grid" id='perf'>
-            <div className="vid-1">
-                <iframe className='embed-vid w-full' src="https://www.youtube.com/embed/Z3sLFa3FkmI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-            </div>
-            <p className="text-white lg:col-start-3">Bach Sonata No. 2 in A minor - Grave</p>
-            <div className="vid-2">
-                <iframe className='embed-vid w-full' src="https://www.youtube.com/embed/vPrTJeND3-k" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-            </div>
-            <div className="vid-3">
-                <iframe className='embed-vid w-full' src="https://www.youtube.com/embed/qP12tqO3ZdM" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="performance-videos h-full w-full section lg:flex lg:flex-col"
+      id="perf"
+    >
+      <div className="flex h-[423px] border">
+        <iframe
+          className="embed-vid w-[750px]"
+          src="https://www.youtube.com/embed/Z3sLFa3FkmI"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+        <p className="text-white">Bach Sonata No. 2 in A minor - Grave</p>
+      </div>
 
+      <div className="vid-2 flex">
+        <iframe
+          className="embed-vid w-2/5"
+          src="https://www.youtube.com/embed/vPrTJeND3-k"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+        <p className="text-white">Ysaye Sonata No. 3 "Ballade"</p>
+      </div>
+      <div className="vid-3 flex">
+        <iframe
+          className="embed-vid w-2/5"
+          src="https://www.youtube.com/embed/qP12tqO3ZdM"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+        <p className="text-white">Bach Partita No. 2 in D minor - Chaconne</p>
+      </div>
+    </div>
+  );
 };
 
 export default PerformanceVideos;
