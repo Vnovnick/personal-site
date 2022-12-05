bbe22369eea1b332cdc23119390c21283a5ffe58
Propnick <safonov.nick10@gmail.com>
2022-12-04 18:27:53 -0800

    remove comment

--- src/Components/Bio/Bio.tsx
+++ src/Components/Bio/Bio.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import image from "./bioImage.jpg";
 import { Link } from "react-router-dom";
 
-// adding a comment
 export default function Bio() {
   return (
     <div className="block mx-[15px] h-5/6 my-auto" id="bio">
