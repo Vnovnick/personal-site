4f3a42032357ff3de69f44c42c082c6265395344
Vnovnick <safonov.nick10@gmail.com>
2024-01-01 18:37:08 -0800

    updating resume links

--- src/Components/Bio/Bio.tsx
+++ src/Components/Bio/Bio.tsx
@@ -92,7 +92,7 @@ export default function Bio() {
           </Link>
         </div>
         <a
-          href="https://drive.google.com/file/d/1SFLxL2B9wNtQq56UctmMKEGAvC60fL26/view?usp=sharing"
+          href="https://drive.google.com/file/d/1SqeODO_SdmCIXXNNUTcB8cssU1HTb0Sy/view?usp=sharing"
           target="_blank"
         >
           <div className="text-white/80 hover:text-white text-lg py-3 px-6 bg-green-900/30 group hover:bg-green-900/50 rounded-2xl transition-all duration-100 w-fit">
